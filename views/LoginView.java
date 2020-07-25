e149f59431ccc4d351fdae23fe3e71f89442a34d
darodriguez2 <darodriguez2@outlook.com>
2020-07-24 21:18:20 -0400

    quick fix

--- views/LoginView.java
+++ views/LoginView.java
@@ -1,74 +1,63 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * LoginView that corresponds with the login UI.
+ * CONTROLLER: AccountController
+ *
+ * @author darod
  */
 package views;
 
 import Utilities.ViewUtility;
 import controllers.AccountController;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
-import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
-import software.amazon.awssdk.services.dynamodb.DynamoDbClientBuilder;
-import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
-import software.amazon.awssdk.services.dynamodb.model.DynamoDbException;
-import software.amazon.awssdk.services.dynamodb.model.GetItemRequest;
 
-/**
- *
- * @author darod
- */
 public class LoginView extends ViewUtility {
-    
+
     @FXML
     private TextField username;
-    
+
     @FXML
     private TextField password;
-    
+
     @FXML
     protected Label errorLabel;
-    
+
     @FXML
     private Label forgotPassword;
-    
+
     @FXML
     private AnchorPane mainAnchorPane;
-       
+
     private AccountController acController = new AccountController();
-    
+
+    protected String uuid;
     public boolean checkLogin() {
         if (this.username.getText().isEmpty() || this.password.getText().isEmpty()) {
             return false;
         } else {
             String result = this.acController.validateLoginRequest(this.username.getText(), this.password.getText());
-            
-            if(result.equals("1")) {
+
+            if (result.equals("1")) {
                 System.out.println("Incorrect username (login view");
                 return false;
-            }
-            else if(result.equals("2")) {
+            } else if (result.equals("2")) {
                 System.out.println("Incorrect password (login view)");
                 return false;
-            }
-            else if(result.equals("3")) {
+            } else if (result.equals("3")) {
                 System.out.println("Database error (login view)");
-            }
-            else{
+            } else {
+                this.uuid = result;
                 return true;
             }
         }
-        
+
         return false;
     }
-    
+
     @FXML
     public void confirm(ActionEvent _event) throws IOException {
         if (checkLogin()) {
@@ -77,23 +66,22 @@ public class LoginView extends ViewUtility {
             this.errorLabel.setText("Incorrect username/password");
         }
     }
-    
+
     public void loadProfiles() {
         //grab all user profiles from aws and store in...Map?
         //idea is to load them at this point so they don't have to be loaded again
         //unless the user adds a new profile. 
         //only want to exectue if checkLogin returns a valid UUID
     }
-    
+
     @FXML
     public void createAccount(ActionEvent _event) throws IOException {
         this.switchScenes(_event, "fxml/CreateAccountFXML.fxml");
     }
-    
+
     @FXML
     public void exit() {
         this.closeApplication(this.mainAnchorPane);
     }
-    
-    
+
 }
